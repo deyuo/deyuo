1bd5ad58e0e09b55f2e4b96868ad869bab5aff46
Mayu <idev.mayu@gmail.com>
2020-05-31 15:53:07 -0500

    semi-final push

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -273,6 +273,19 @@ $( document ).ready(function() {
 
   }
 
+  var otherCheckbox = document.querySelector('input[value="other"]');
+  var otherText = document.querySelector('input[id="otherValue"]');
+  otherText.style.visibility = 'hidden';
+
+  otherCheckbox.addEventListener('change', () => {
+    if(otherCheckbox.checked) {
+      otherText.style.visibility = 'visible';
+      otherText.value = '';
+    } else {
+      otherText.style.visibility = 'hidden';
+    }
+  });
+
   outerNav();
   workSlider();
   transitionLabels();
